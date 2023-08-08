c1c2e7c8fe56e7845e9b43ff874c209d278b5fb1
nvp85 <nvp@zhukov.al>
2023-08-07 23:31:12 -0700

    add a filter dict_get to get an event name by its date form a dict in schedule.html template

--- main/templatetags/filters.py
+++ main/templatetags/filters.py
@@ -5,3 +5,7 @@ register = template.Library()
 @register.filter(name="add_class")
 def add_class(field, cls):
     return field.as_widget(attrs={"class": cls})
+
+@register.filter(name='dict_get')
+def dict_get(d, k):
+    return d[k]
